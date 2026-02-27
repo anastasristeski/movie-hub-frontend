eb378a612197dd29d682ca876bcea5549a9e0208
anastasristeski <anastas.risteski5@gmail.com>
2026-02-27 06:50:13 +0100

    redeploy

--- src/lib/api/axios.js
+++ src/lib/api/axios.js
@@ -1,5 +1,6 @@
-import axios from "axios";
 import { clearAccessToken, getAccessToken, setAccessToken } from "./auth";
+
+import axios from "axios";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
   withCredentials: true,
