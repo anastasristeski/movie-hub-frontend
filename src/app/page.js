019b15a46bdc6e2ac99b8777255a1249d897b672
anastasristeski <anastas.risteski5@gmail.com>
2026-02-27 03:21:51 +0100

    base-url changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import Hero from "@/components/hero";
 
 import TrendingMoviesRow from "@/components/movie/trending-movies-row";
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
+
 async function fetchTrending(type) {
-  const response = await fetch(`http://localhost:8080/api/trending/${type}`, {
+  const response = await fetch(`${BASE_URL}/trending/${type}`, {
     cache: "no-store",
   });
   if (!response.ok) throw new Error("Failed to fetch trening movies");
--- src/lib/api/axios.js
+++ src/lib/api/axios.js
@@ -31,6 +31,6 @@ api.interceptors.response.use(
       }
     }
     return Promise.reject(error);
-  }
+  },
 );
 export default api;
