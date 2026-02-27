a405d4ce52af447d19e909f21d2eda7ed47e0123
anastasristeski <anastas.risteski5@gmail.com>
2026-02-27 04:40:08 +0100

    fixing reference error

--- next.config.js
+++ next.config.js
@@ -1,23 +1,15 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "image.tmdb.org",
-        pathname: "/t/p/**",
-      },
-    ],
-  },
-  reactCompiler: true,
-};
-
-export default nextConfig;
-const path = require("path");
-
-module.exports = {
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
 };
+
+export default nextConfig;
