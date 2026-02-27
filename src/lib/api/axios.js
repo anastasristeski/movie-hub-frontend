71cb2e35e2145e8cc283768a6ddf3b0ce4a14b68
anastasristeski <anastas.risteski5@gmail.com>
2026-02-27 21:35:16 +0100

    env variable test

--- src/lib/api/axios.js
+++ src/lib/api/axios.js
@@ -1,6 +1,9 @@
 import { clearAccessToken, getAccessToken, setAccessToken } from "./auth";
 
 import axios from "axios";
+
+console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
+
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
   withCredentials: true,
