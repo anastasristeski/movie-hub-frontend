6ca687b15cb7f9b50668f15110a7d9350574783f
anastasristeski <anastas.risteski5@gmail.com>
2026-02-27 04:08:39 +0100

    fixed json paths

--- next.config.mjs
+++ next.config.mjs
@@ -13,3 +13,11 @@ const nextConfig = {
 };
 
 export default nextConfig;
+const path = require("path");
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
